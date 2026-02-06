cd00e11656c98958bfcdc8d8a03b3915cb31f174
Cong Vo <43516400+congvmit@users.noreply.github.com>
2026-02-06 10:56:32 +0700

    Handle mapbox token in tile_url

--- geovideo/providers/base.py
+++ geovideo/providers/base.py
@@ -10,5 +10,10 @@ class MapProvider:
     name: str
     tile_url_template: str
 
-    def tile_url(self, z: int, x: int, y: int) -> str:
-        return self.tile_url_template.format(z=z, x=x, y=y)
+    def tile_url(self, z: int, x: int, y: int, token: str | None = None, **kwargs: str) -> str:
+        format_args: dict[str, str | int] = {"z": z, "x": x, "y": y, **kwargs}
+        if "{token}" in self.tile_url_template:
+            if token is None:
+                raise ValueError(f"Token required for provider '{self.name}'.")
+            format_args["token"] = token
+        return self.tile_url_template.format(**format_args)
